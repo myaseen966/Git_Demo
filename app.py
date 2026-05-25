a1aade07dc812da6114c738df2d89a1f82630424
Yaseen Hayat <muhammadyaseenhayat03@gmail.com>
2026-05-25 20:16:20 +0500

    wrong commit

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ birth_year = current_year - age
 # Output
 print(name +  is  + str(age) +  years old.)
 print(She was born in  + str(birth_year) + .)
+/
+def calculate_average(numbers)
+    total = 0
+    for i in range(len(numbers)):
+        total = total + numbers[i+1]
+    
+    average = Total / len(numbers)
+    return average
+
+print(calculate_average([10, 20, 30]))
